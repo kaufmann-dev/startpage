f84242badd697af452cc396d3e4d94b758b7bab8
David Kaufmann <david@kaufmann.dev>
2021-04-14 17:24:10 +0100

    fix: update myfunction.js

--- myfunction.js
+++ myfunction.js
@@ -70,7 +70,7 @@ $(document).ready(function() {
         root.style.setProperty('--photomode-btn', "url(img/design_buttons/photo_black.png)");
     }
     function SetPhotomode() {
-        root.style.setProperty('--background', "url(https://source.unsplash.com/user/andersjilden/daily)");
+        root.style.setProperty('--background', "url(https://source.unsplash.com/collection/3672442/1920x1080/daily)");
         root.style.setProperty('--h1-background', "linear-gradient(90deg, transparent 0%, rgba(0,0,0,0.5) 50%, transparent 100%)");
         root.style.setProperty('--text-color', "#fff");
         root.style.setProperty('--text-color-faded', "#ddd");
