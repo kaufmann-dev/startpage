715da8bff2133187543ddb58bc26426d83a160a9
David Kaufmann <david@kaufmann.dev>
2021-06-07 21:29:24 +0100

    feat: update img/arrow/

--- myfunction.js
+++ myfunction.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     let time = new Date();
     $.getJSON('apps.json', function(data) {
         data.apps.forEach(element => {
-            $('#apps').append("<a href='" + element.link + "'><div class='app'><div class='imgWrapper'><img src='" + element.image + "' title='" + element.name + "'></div><p>" + element.name + "</p></div></a>");
+            $('#apps').append("<a href='" + element.link + "'><div class='app shadow rounded'><div class='imgWrapper shadow rounded-full'><img src='" + element.image + "' title='" + element.name + "'></div><p>" + element.name + "</p></div></a>");
         });
     });
     $('header h1').text(pad(time.getHours()) + ":" + pad(time.getMinutes()) + ":" + pad(time.getSeconds()));
@@ -46,39 +46,48 @@ $(document).ready(function() {
         SetPhotomode();
     });
     function SetDarkmode() {
+        root.style.setProperty('--fallback-background', "#1e1e1e");
         root.style.setProperty('--background', "#1e1e1e");
-        root.style.setProperty('--h1-background', "linear-gradient(90deg, transparent 0%, #393939 50%, transparent 100%)");
+        root.style.setProperty('--h1-background', "linear-gradient(90deg, transparent 0%, #282828 50%, transparent 100%)");
         root.style.setProperty('--text-color', "#fff");
         root.style.setProperty('--text-color-faded', "#ddd");
         root.style.setProperty('--light-contrast', "#282828");
         root.style.setProperty('--light-contrast-fixed', "#282828");
         root.style.setProperty('--high-contrast', "#393939");
-        root.style.setProperty('--darkmode-btn', "url(img/design_buttons/dark_white.png)");
-        root.style.setProperty('--lightmode-btn', "url(img/design_buttons/light_white.png)");
-        root.style.setProperty('--photomode-btn', "url(img/design_buttons/photo_white.png)");
+        root.style.setProperty('--arrow', "url(img/arrow/arrow_drop_down_white_24dp.svg)");
+        
+        $(".lightmode img").attr("src","img/design_buttons/light_white.png");
+        $(".darkmode img").attr("src","img/design_buttons/dark_white.png");
+        $(".photomode img").attr("src","img/design_buttons/photo_white.png");
     }
     function SetLightmode() {
-        root.style.setProperty('--background', "#fff");
-        root.style.setProperty('--h1-background', "linear-gradient(90deg, transparent 0%, #ddd 50%, transparent 100%)");
+        root.style.setProperty('--fallback-background', "#f7f7f7");
+        root.style.setProperty('--background', "#f7f7f7");
+        root.style.setProperty('--h1-background', "linear-gradient(90deg, transparent 0%, #fff 50%, transparent 100%)");
         root.style.setProperty('--text-color', "#000");
         root.style.setProperty('--text-color-faded', "#444");
-        root.style.setProperty('--light-contrast', "#eee");
-        root.style.setProperty('--light-contrast-fixed', "#eee");
-        root.style.setProperty('--high-contrast', "#ddd");
-        root.style.setProperty('--darkmode-btn', "url(img/design_buttons/dark_black.png)");
-        root.style.setProperty('--lightmode-btn', "url(img/design_buttons/light_black.png)");
-        root.style.setProperty('--photomode-btn', "url(img/design_buttons/photo_black.png)");
+        root.style.setProperty('--light-contrast', "#fff");
+        root.style.setProperty('--light-contrast-fixed', "#fff");
+        root.style.setProperty('--high-contrast', "#ebebeb");
+        
+        $(".lightmode img").attr("src","img/design_buttons/light_black.png");
+        $(".darkmode img").attr("src","img/design_buttons/dark_black.png");
+        $(".photomode img").attr("src","img/design_buttons/photo_black.png");
+        root.style.setProperty('--arrow', "url(img/arrow/arrow_drop_down_black_24dp.svg)");
     }
     function SetPhotomode() {
+        root.style.setProperty('--fallback-background', "#1e1e1e");
         root.style.setProperty('--background', "url(https://source.unsplash.com/collection/3672442/1920x1080/daily)");
-        root.style.setProperty('--h1-background', "linear-gradient(90deg, transparent 0%, rgba(0,0,0,0.5) 50%, transparent 100%)");
+        root.style.setProperty('--h1-background', "linear-gradient(90deg, transparent 0%, rgba(0,0,0,0.3) 50%, transparent 100%)");
         root.style.setProperty('--text-color', "#fff");
         root.style.setProperty('--text-color-faded', "#ddd");
         root.style.setProperty('--light-contrast', "rgba(0,0,0,0.3)");
         root.style.setProperty('--light-contrast-fixed', "#282828");
         root.style.setProperty('--high-contrast', "rgba(0,0,0,0.5)");
-        root.style.setProperty('--darkmode-btn', "url(img/design_buttons/dark_white.png)");
-        root.style.setProperty('--lightmode-btn', "url(img/design_buttons/light_white.png)");
-        root.style.setProperty('--photomode-btn', "url(img/design_buttons/photo_white.png)");
+        root.style.setProperty('--arrow', "url(img/arrow/arrow_drop_down_white_24dp.svg)");
+
+        $(".lightmode img").attr("src","img/design_buttons/light_white.png");
+        $(".darkmode img").attr("src","img/design_buttons/dark_white.png");
+        $(".photomode img").attr("src","img/design_buttons/photo_white.png");
     }
 });
